56a517839d207dd51ccf72259655a66021ea6703
vitor <vitor@DESKTOP-4OIHHQ9>
2023-04-26 16:07:23 -0300

    Add project files.

---
+++ exercicio classe/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_classe
+{
+    internal class Pessoa
+    {
+
+        public string Nome;
+        public int Idade;
+
+    }
+}
---
+++ exercicio classe/Program.cs
@@ -0,0 +1,33 @@
+﻿using exercicio_classe;
+
+Pessoa p1, p2;
+
+p1 = new Pessoa();
+p2 = new Pessoa();
+
+Console.Write("Informe aqui os dados da primeira pessoa.\r\nNome: ");
+p1.Nome = Console.ReadLine();
+Console.Write("Idade: ");
+p1.Idade = int.Parse(Console.ReadLine());
+
+Console.Write("Informe aqui os dados da segunda pessoa.\r\nNome: ");
+p2.Nome = Console.ReadLine();
+Console.Write("Idade: ");
+p2.Idade = int.Parse(Console.ReadLine());
+
+if (p1.Idade > p2.Idade)
+{
+
+    Console.WriteLine($"{p1.Nome} é mais velho(a) que {p2.Nome}!");
+
+} else if (p1.Idade < p2.Idade)
+{
+
+    Console.WriteLine($"{p2.Nome} é mais velho(a) que {p1.Nome}!");
+
+} else
+{
+
+    Console.WriteLine($"{p1.Nome} e {p2.Nome} tem a mesma idade!");
+
+}
